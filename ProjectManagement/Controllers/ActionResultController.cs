c593ecf7a2cf3fd83f3389821847c9f8e6f1ba61
Diana Onet <Diana Onet>
2017-01-18 22:59:23 +0200

    Curs6 Ex8

---
+++ ProjectManagement/Controllers/ActionResultController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectManagement.Controllers
+{
+    public class ActionResultController : Controller
+    {
+        public ActionResult RedirectPermanent()
+        {
+            return Content("This message is sent from the Index action!”;");
+        }
+        public ActionResult RedirectToActionInTheSameController()
+        {
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
